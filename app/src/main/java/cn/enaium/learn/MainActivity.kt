566f3c9b5777fca776803a283c51475c77f4df04
Enaium <wyy-666@hotmail.com>
2022-04-21 08:48:24 +0800

    Update MainActivity.kt

--- app/src/main/java/cn/enaium/learn/MainActivity.kt
+++ app/src/main/java/cn/enaium/learn/MainActivity.kt
@@ -1,33 +1,20 @@
 package cn.enaium.learn
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.selection.DisableSelection
-import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cn.enaium.learn.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,11 +34,10 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Column(
-        Modifier.height(100.dp), verticalArrangement
-        = Arrangement.SpaceBetween
-    ) {
-        Text("One line")
-        Text("Two line")
+    Row(modifier = Modifier.width(200.dp)
+        , horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text("One")
+        Text("Two")
+        Text("Three")
     }
 }
